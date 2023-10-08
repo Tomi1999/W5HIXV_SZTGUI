e10b2896482cfb79fab1d4abbf23cf0d856637f6
Tomi1999 <daroczitamas99@gmail.com>
2023-10-08 19:26:41 +0200

    CarRepository created

---
+++ W5HIXV_HFT_2023241.Repository/CarRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using W5HIXV_HFT_2023241.Models;
+
+namespace W5HIXV_HFT_2023241.Repository
+{
+    public class CarRepository : Repository<Car>, IRepository<Car>
+    {
+        public CarRepository(FleetDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Car Read(int id)
+        {
+            ;
+        }
+
+        public override void Update(Car item)
+        {
+           ;
+        }
+    }
+}

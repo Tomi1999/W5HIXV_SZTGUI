8e1319c2585eaccd6646562b9d2d232acc2630f1
Tomi1999 <daroczitamas99@gmail.com>
2023-10-08 20:05:23 +0200

    Interface finished

--- W5HIXV_HFT_2023241.Logic/IDriverLogic.cs
+++ W5HIXV_HFT_2023241.Logic/IDriverLogic.cs
@@ -1,6 +1,14 @@
-﻿namespace W5HIXV_HFT_2023241.Logic
+﻿using System.Linq;
+using W5HIXV_HFT_2023241.Models;
+
+namespace W5HIXV_HFT_2023241.Logic
 {
     public interface IDriverLogic
     {
+        public void Create(Driver item);
+        public void Delete(int id);
+        public Driver Read(int id);
+        public IQueryable<Driver> ReadAll();
+        public void Update(Driver item);
     }
 }

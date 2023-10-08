d272538fbdd995b7e969e8da9807ef6ccc9c4464
Tomi1999 <daroczitamas99@gmail.com>
2023-10-08 16:07:28 +0200

    Driver class added

---
+++ W5HIXV_HFT_2023241.Models/Driver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W5HIXV_HFT_2023241.Models
+{
+    public class Driver
+    {
+        public int Id {  get; set; }    
+
+        public string Name { get; set; }
+
+        public int Distance { get; set; }
+    }
+}

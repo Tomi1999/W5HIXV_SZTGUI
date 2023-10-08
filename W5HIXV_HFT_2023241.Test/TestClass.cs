8b4e03aadaa4d4f2e372d032649d9d8df1f7cee6
Tomi1999 <daroczitamas99@gmail.com>
2023-10-08 20:20:04 +0200

    class updated

--- W5HIXV_HFT_2023241.Test/TestClass.cs
+++ W5HIXV_HFT_2023241.Test/TestClass.cs
@@ -1,19 +1,33 @@
 ﻿using Moq;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
+using W5HIXV_HFT_2023241.Logic;
 using W5HIXV_HFT_2023241.Models;
 using W5HIXV_HFT_2023241.Repository;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace W5HIXV_HFT_2023241.Test
 {
     [TestFixture]
     public class TestClass
     {
-        CarRepository carRepo;
-        DriverRepository driverRepo;
-        SiteRepository siteRepo;
+        CarLogic carLogic;
+        DriverLogic driverLogic;
+        SiteLogic siteLogic;
         Mock<IRepository<Car>> carMock;
         Mock<IRepository<Driver>> driverMock;
         Mock<IRepository<Site>> siteMock;
+        
+        [SetUp]
+        public void InitCar()
+        {
+            carMock = new Mock<IRepository<Car>>();
+            carMock.Setup(m => m.ReadAll()).Returns(new List<Car>()
+            {
+                 
+                }.AsQueryable());
+            carLogic = new CarLogic(carMock.Object);
+        }
     }
 }

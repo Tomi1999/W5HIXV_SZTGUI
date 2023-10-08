daf6e9cbe77575b292aab88b778b349dc01ae580
Tomi1999 <daroczitamas99@gmail.com>
2023-10-08 20:13:03 +0200

    moq added

--- W5HIXV_HFT_2023241.Test/TestClass.cs
+++ W5HIXV_HFT_2023241.Test/TestClass.cs
@@ -1,8 +1,19 @@
-﻿using System;
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using W5HIXV_HFT_2023241.Models;
+using W5HIXV_HFT_2023241.Repository;
 
 namespace W5HIXV_HFT_2023241.Test
 {
+    [TestFixture]
     public class TestClass
     {
+        CarRepository carRepo;
+        DriverRepository driverRepo;
+        SiteRepository siteRepo;
+        Mock<IRepository<Car>> carMock;
+        Mock<IRepository<Driver>> driverMock;
+        Mock<IRepository<Site>> siteMock;
     }
 }

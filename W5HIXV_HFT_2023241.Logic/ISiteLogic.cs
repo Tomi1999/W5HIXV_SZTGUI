84a9c65eb3acac041a8f3069f440f6f900cb8813
Tomi1999 <daroczitamas99@gmail.com>
2023-10-08 19:57:41 +0200

    Interface finished

--- W5HIXV_HFT_2023241.Logic/ISiteLogic.cs
+++ W5HIXV_HFT_2023241.Logic/ISiteLogic.cs
@@ -1,6 +1,16 @@
-﻿namespace W5HIXV_HFT_2023241.Logic
+﻿using System.Linq;
+using W5HIXV_HFT_2023241.Models;
+using W5HIXV_HFT_2023241.Repository;
+
+namespace W5HIXV_HFT_2023241.Logic
 {
     public interface ISiteLogic
     {
+
+        public void Create(Site item);
+        public void Delete(int id);
+        public Site Read(int id);
+        public IQueryable<Site> ReadAll();
+        public void Update(Site item);
     }
 }

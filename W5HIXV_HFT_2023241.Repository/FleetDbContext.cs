21840ca7ebffe8e8dd745d12cefb0b6cf885e097
Tomi1999 <daroczitamas99@gmail.com>
2023-10-08 16:35:34 +0200

    dbcontext file created

---
+++ W5HIXV_HFT_2023241.Repository/FleetDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using W5HIXV_HFT_2023241.Models;
+
+namespace W5HIXV_HFT_2023241.Repository
+{
+    public class FleetDbContext : DbContext
+    {
+
+    }
+}

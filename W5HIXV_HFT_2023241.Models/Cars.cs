62431cc1dbaa21ed1b2c76d3ad1b15c5736c869d
Tomi1999 <daroczitamas99@gmail.com>
2023-10-08 16:05:11 +0200

    Cars updated

--- W5HIXV_HFT_2023241.Models/Cars.cs
+++ W5HIXV_HFT_2023241.Models/Cars.cs
@@ -6,12 +6,16 @@ using System.Threading.Tasks;
 
 namespace W5HIXV_HFT_2023241.Models
 {
-    internal class Cars
+    public class Cars
     {
         public int Id { get; set; }
 
         public string Plate {  get; set; }  
 
         public string Type { get; set; } 
+
+        public string Brand { get; set; } 
+
+        public int Total_Weith { get; set; }
     }
 }

b875f0fc634610d6df18e1ce4d35ffca89c63c5b
Tomi1999 <daroczitamas99@gmail.com>
2023-10-08 19:56:12 +0200

    Crud Methods finished

---
+++ W5HIXV_HFT_2023241.Logic/SiteLogic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using W5HIXV_HFT_2023241.Models;
+using W5HIXV_HFT_2023241.Repository;
+
+namespace W5HIXV_HFT_2023241.Logic
+{
+    public class SiteLogic : ISiteLogic
+    {
+        IRepository<Site> repo;
+
+        public SiteLogic(IRepository<Site> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Site item)
+        {
+            this.repo.Create(item); 
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Site Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Site> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Site item) 
+        {
+            this.repo.Update(item); 
+        }
+    }
+}

408641d2880c2e03fb089b4d575338f4784efc80
Tomi1999 <daroczitamas99@gmail.com>
2023-10-08 20:01:40 +0200

    Interface finished

--- W5HIXV_HFT_2023241.Logic/ICarLogic.cs
+++ W5HIXV_HFT_2023241.Logic/ICarLogic.cs
@@ -1,6 +1,16 @@
-﻿namespace W5HIXV_HFT_2023241.Logic
+﻿using System.Linq;
+using W5HIXV_HFT_2023241.Models;
+using W5HIXV_HFT_2023241.Repository;
+
+namespace W5HIXV_HFT_2023241.Logic
 {
     public interface ICarLogic
     {
+
+        public void Create(Car item);
+        public void Delete(int id);
+        public Car Read(int id);
+        public IQueryable<Car> ReadAll();
+        public void Update(Car item);
     }
 }

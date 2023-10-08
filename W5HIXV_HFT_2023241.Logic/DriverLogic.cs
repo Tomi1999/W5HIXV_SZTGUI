76612302ef953e0f92d1cb4f9b47bc929253ca99
Tomi1999 <daroczitamas99@gmail.com>
2023-10-08 20:03:43 +0200

    Class finished

---
+++ W5HIXV_HFT_2023241.Logic/DriverLogic.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using W5HIXV_HFT_2023241.Models;
+using W5HIXV_HFT_2023241.Repository;
+
+namespace W5HIXV_HFT_2023241.Logic
+{
+    public class DriverLogic
+    {
+        IRepository<Driver> repo;
+
+        public DriverLogic(IRepository<Driver> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Driver item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Driver Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Driver> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Driver item)
+        {
+            this.repo.Update(item);
+        }
+
+    }
+}

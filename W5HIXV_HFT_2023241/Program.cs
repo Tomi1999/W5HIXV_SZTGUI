4af59567e1c4dfdade3534b9193d0b0636274b38
Tomi1999 <daroczitamas99@gmail.com>
2023-10-08 14:59:15 +0200

    Add project files.

---
+++ W5HIXV_HFT_2023241/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace W5HIXV_HFT_2023241
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

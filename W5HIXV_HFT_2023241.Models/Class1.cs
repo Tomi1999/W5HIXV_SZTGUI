3456e87e5217e5cee72aa3ae6f7799e4113c1930
Tomi1999 <daroczitamas99@gmail.com>
2023-10-08 16:00:52 +0200

    site class added

--- W5HIXV_HFT_2023241.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace W5HIXV_HFT_2023241.Models
-{
-    public class Class1
-    {
-    }
-}
---
+++ W5HIXV_HFT_2023241.Models/Site.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W5HIXV_HFT_2023241.Models
+{
+    public class Site
+    {
+        public int Id { get; set; } 
+
+        public string Name { get; set; }    
+
+        public string Address { get; set; }
+        
+    }
+}

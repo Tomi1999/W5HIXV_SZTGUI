fc6c103d4f92d64937db346e3462e3da5b7251e1
Tomi1999 <daroczitamas99@gmail.com>
2023-10-08 19:38:27 +0200

    Class created

---
+++ W5HIXV_HFT_2023241.Repository/DriverRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using W5HIXV_HFT_2023241.Models;
+
+namespace W5HIXV_HFT_2023241.Repository
+{
+    public class DriverRepository : Repository<Driver>, IRepository<Driver>
+    {
+        public DriverRepository(FleetDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Driver Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(Driver item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

a06dc689dd7d441dd1a0ebd1c3efdacd94ec2fe9
Tomi1999 <daroczitamas99@gmail.com>
2023-10-08 16:02:38 +0200

    Class added

---
+++ W5HIXV_HFT_2023241.Models/Cars.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W5HIXV_HFT_2023241.Models
+{
+    internal class Cars
+    {
+        public int Id { get; set; }
+
+        public string Plate {  get; set; }  
+
+        public string Type { get; set; } 
+    }
+}

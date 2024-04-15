abdb9199a9e7a0e9761c7b8176c10cd5ce3a6468
Tomi1999 <daroczitamas99@gmail.com>
2024-04-15 22:00:29 +0200

    mainwindow updated

---
+++ W5HIXV.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using W5HIXV_HFT_2023241.Models;
+
+namespace W5HIXV.WpfClient
+{
+    public class MainWindowViewModel
+    {
+        public RestCollection<Car> Cars { get; set; }
+        public MainWindowViewModel()
+        {
+            Cars = new RestCollection<Car>("http://localhost:55762/", "car");
+        }
+    }
+}

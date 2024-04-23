06852833f6783ffe23c1288e88ea3f7a630fe143
Tomi1999 <daroczitamas99@gmail.com>
2024-04-23 18:20:25 +0200

    class trying

--- W5HIXV.WpfClient/MainWindowViewModel.cs
+++ W5HIXV.WpfClient/MainWindowViewModel.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using W5HIXV_HFT_2023241.Models;
 
 namespace W5HIXV.WpfClient
 {
     public class MainWindowViewModel
     {
+
+        public ICommand CreateCarCommand { get; set; }  
+        public ICommand DeleteCarCommand { get; set; }  
+        public ICommand UpdateCarCommand { get; set; }  
         public RestCollection<Car> Cars { get; set; }
         public MainWindowViewModel()
         {
+            List<string> models = new List<string>() {"Car", "Driver", "Site" };
             Cars = new RestCollection<Car>("http://localhost:55762/", "car");
         }
     }

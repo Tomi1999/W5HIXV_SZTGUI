97773c2c0c9fe689140f9b465fd4570210d564b9
Tomi1999 <daroczitamas99@gmail.com>
2024-04-23 22:31:29 +0200

    Carwindow buttons finished

--- W5HIXV.WpfClient/CarwinowViewModel.cs
+++ W5HIXV.WpfClient/CarwinowViewModel.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace W5HIXV.WpfClient
 {
     class CarwinowViewModel
     {
+        public ICommand ListAllCar { get; set; }
+
+        public ICommand CreateCar { get; set; }
+
+        public ICommand ReadCar { get; set; }
+
+        public ICommand UpdateCar { get; set; }
+
+        public ICommand DeleteCar { get; set; }
+
     }
 }

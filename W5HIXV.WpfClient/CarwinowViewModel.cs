e9ccc64b9d6ea83f4ba3408fbf3a9561f67ef327
hallgato <hallgato@C202-14>
2024-04-25 13:21:41 +0200

    ctor added

--- W5HIXV.WpfClient/CarwinowViewModel.cs
+++ W5HIXV.WpfClient/CarwinowViewModel.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using W5HIXV_HFT_2023241.Models;
 
 namespace W5HIXV.WpfClient
 {
-    class CarwinowViewModel
+    internal class CarwinowViewModel
     {
+        public RestCollection<Car> Cars { get; set; } 
+        
         public ICommand ListAllCar { get; set; }
 
         public ICommand CreateCar { get; set; }
@@ -18,6 +21,11 @@ namespace W5HIXV.WpfClient
         public ICommand UpdateCar { get; set; }
 
         public ICommand DeleteCar { get; set; }
+
+        public CarwinowViewModel()
+        {
+            Cars = new RestCollection<Car>("http://localhost:55762/", "car");
+        }
 
     }
 }

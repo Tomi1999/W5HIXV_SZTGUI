0fc4b9681635bce244e503bdf25c091e7a9fba54
Tomi1999 <daroczitamas99@gmail.com>
2024-05-05 19:36:22 +0200

    finished now

--- W5HIXV_SZTGUI.JsClient/wwwroot/Site.js
+++ W5HIXV_SZTGUI.JsClient/wwwroot/Site.js
@@ -63,14 +63,14 @@ function display() {
     document.getElementById("resultarea").innerHTML = "";
     cars.forEach(t => {
         document.getElementById("resultarea").innerHTML +=
-            "<tr><td>" + t.id + "</td><td>" + t.Size + "</td><td>" + t.address + "</td></tr>"
+            "<tr><td>" + t.id + "</td><td>" + t.size + "</td><td>" + t.address + "</td></tr>"
     })
 }
 function update() {
     let size = document.getElementById("sizenew").value;
     let id = document.getElementById("id_update").value;
-    let brandnew = document.getElementById("brandnew").value;
-    let total_Weithnew = document.getElementById("total_Weithnew").value;
+    let address = document.getElementById("addressnew").value;
+    let city = document.getElementById("citynew").value;
 
     fetch('http://localhost:55762/Site/' + id, {
         method: 'DELETE',
@@ -88,7 +88,7 @@ function update() {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', },
         body: JSON.stringify(
-            { size: size, brand: brandnew, total_Weith: total_Weithnew, id:id })
+            { size: size, address : address, city: city, id:id })
     })
         .then(response => response)
         .then(data => {
@@ -98,14 +98,14 @@ function update() {
         .catch((error) => { console.error('Error:', error); });
 }
 function create() {
-    let plate = document.getElementById("plate").value;
-    let brand = document.getElementById("brand").value;
-    let total_Weith = document.getElementById("total_Weith").value;
+    let size = document.getElementById("size").value;
+    let address = document.getElementById("address").value;
+    let city = document.getElementById("city").value;
     fetch('http://localhost:55762/Site', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', },
         body: JSON.stringify(
-            { plate: plate, brand: brand, total_Weith: total_Weith })
+            { size: size, address: address, city: city })
     })
         .then(response => response)
         .then(data => {

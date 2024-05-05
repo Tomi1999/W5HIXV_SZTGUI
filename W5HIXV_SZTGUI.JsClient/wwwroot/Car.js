526d099ad6adefde3617baedecde42ebd10c1a67
Tomi1999 <daroczitamas99@gmail.com>
2024-05-05 17:43:51 +0200

    car almost finished

--- W5HIXV_SZTGUI.JsClient/wwwroot/Car.js
+++ W5HIXV_SZTGUI.JsClient/wwwroot/Car.js
@@ -1,29 +1,79 @@
 ﻿let cars = [];
-fetch('http://localhost:55762/Car')
-    .then(x => x.json())
-    .then(y => {
-        cars = y;
-        console.log(y);
-        display();
+let connection = null;
+getData();
+setupSignalR();
+
+
+function setupSignalR() {
+    connection = new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:55762/hub")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    connection.on("ActorCreated", (user, message) => {
+        getdata();
     });
 
+    connection.on("ActorDeleted", (user, message) => {
+        getdata();
+    });
+
+    connection.onclose(async () => {
+        await start();
+    });
+    start();
+
+
+}
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
+async function getData() {
+    await fetch('http://localhost:55762/Car')
+        .then(x => x.json())
+        .then(y => {
+            cars = y;
+            console.log(y);
+            display();
+        });
+
+}
+
+function remove(id) {
+    fetch('http://localhost:55762/Car/' + id, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json', },
+        body: null
+    })
+        .then(response => response)
+        .then(data => {
+            getData();
+        })
+        .catch((error) => { console.error('Error:', error); });
+}
 function display() {
+    document.getElementById("resultarea").innerHTML = "";
     cars.forEach(t => {
         document.getElementById("resultarea").innerHTML +=
             "<tr><td>" + t.id + "</td><td>" + t.plate + "</td><td>" + t.brand + "</td><td>" + t.total_Weith + "</td></tr>"
-        console.log(t.plate)
     })
 }
 function create() {
     let plate = document.getElementById("plate").value; 
     let brand = document.getElementById("brand").value; 
     let total_Weith = document.getElementById("total_Weith").value; 
-    let id = document.getElementById("id").value; 
     fetch('http://localhost:55762/Car', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', },
         body: JSON.stringify(
-            {id:id plate: plate, brand: brand, total_Weith: total_Weith })
+            {plate: plate, brand: brand, total_Weith: total_Weith })
     })
         .then(response => response)
         .then(data => {
@@ -31,5 +81,4 @@ function create() {
             getdata();
         })
         .catch((error) => { console.error('Error:', error); });
-
 }

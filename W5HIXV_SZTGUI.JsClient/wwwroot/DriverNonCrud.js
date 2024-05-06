0b7fa30bdbe75313f98e2e814920a534ee6aec58
Tomi1999 <daroczitamas99@gmail.com>
2024-05-06 22:20:01 +0200

    NonCrud page finished

---
+++ W5HIXV_SZTGUI.JsClient/wwwroot/DriverNonCrud.js
@@ -0,0 +1,30 @@
+﻿let drivers = [];
+
+async function getData() {
+    const inputValue = document.getElementById("inputValue").value;
+    const url = `http://localhost:55762/DriverNon/DriversOverValue?value=${inputValue}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        drivers = data;
+        display();
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+function display() {
+    const resultArea = document.getElementById("resultarea");
+    resultArea.innerHTML = "";
+
+    drivers.forEach(driver => {
+        resultArea.innerHTML += `
+            <tr>
+                <td>${driver.id}</td>
+                <td>${driver.name}</td>
+                <td>${driver.distance}</td>
+            </tr>
+        `;
+    });
+}

f303e7fa6da142d3ed6b9ad6e0d236b9e7a05b50
Tomi1999 <daroczitamas99@gmail.com>
2024-05-07 16:17:04 +0200

    Files finished

---
+++ W5HIXV_SZTGUI.JsClient/wwwroot/CarNonCrud.js
@@ -0,0 +1,45 @@
+﻿let cars = [];
+
+async function getCarsByBrand() {
+    const inputValue = document.getElementById("inputValue").value;
+    const url = `http://localhost:55762/CarNon/GetBrands?brand=${inputValue}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        cars = data;
+        display();
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+async function getCarsOverTW() {
+    const inputValue = document.getElementById("inputValue").value;
+    const url = `http://localhost:55762/CarNon/CarsOverTW?weith=${inputValue}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        cars = data;
+        display();
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+function display() {
+    const resultArea = document.getElementById("resultarea");
+    resultArea.innerHTML = "";
+
+    cars.forEach(car => {
+        resultArea.innerHTML += `
+            <tr>
+                <td>${car.id}</td>
+                <td>${car.plate}</td>
+                <td>${car.brand}</td>
+                <td>${car.total_Weith}</td>
+            </tr>
+        `;
+    });
+}
---
+++ W5HIXV_SZTGUI.JsClient/wwwroot/SiteNonCrud.js
@@ -0,0 +1,45 @@
+﻿let sites = [];
+
+async function getSitesInCity() {
+    const inputValueCity = document.getElementById("inputValueCity").value;
+    const url = `http://localhost:55762/SiteNon/SiteInCity?city=${inputValueCity}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        sites = data;
+        display();
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+async function getSitesBySize() {
+    const inputValueSize = document.getElementById("inputValueSize").value;
+    const url = `http://localhost:55762/SiteNon/SitesSize?size=${inputValueSize}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        sites = data;
+        display();
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+function display() {
+    const resultArea = document.getElementById("resultarea");
+    resultArea.innerHTML = "";
+
+    sites.forEach(site => {
+        resultArea.innerHTML += `
+            <tr>
+                <td>${site.id}</td>
+                <td>${site.size}</td>
+                <td>${site.address}</td>
+                <td>${site.city}</td>
+            </tr>
+        `;
+    });
+}
